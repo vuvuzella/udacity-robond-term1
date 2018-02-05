46e734d88aa2c088799c42c747ffb5e8ba588f60
vuvuzella <j.tabacii@gmail.com>
2018-02-05 14:39:03 +1100

    Added fashion mnist data lab notebook

---
+++ convolutional_neural_networks/cnn_in_tensorflow/tf_max_pool.py
@@ -0,0 +1,37 @@
+"""
+Set the values to `strides` and `ksize` such that
+the output shape after pooling is (1, 2, 2, 1).
+"""
+import tensorflow as tf
+import numpy as np
+
+# `tf.nn.conv2d` requires the input be 4D (batch_size, height, width, depth)
+# (1, 4, 4, 1)
+x = np.array([
+    [0, 1, 0.5, 10],
+    [2, 2.5, 1, -8],
+    [4, 0, 5, 6],
+    [15, 1, 2, 3]
+], dtype=np.float32).reshape((1, 4, 4, 1))
+
+X = tf.constant(x)
+
+def maxpool(inp):
+    # TODO: Set the ksize (filter size) for each dimension (batch_size,
+    # height, width, depth)
+    ksize = [1,2,2,1]
+    # TODO: Set the stride for each dimension (batch_size, height, width, 
+    # depth)
+    strides = [1, 2, 2, 1]
+    # TODO: set the padding, either 'VALID' or 'SAME'.
+    padding = 'SAME'
+    # https://www.tensorflow.org/versions/r0.11/api_docs/python/nn.html#max_pool
+    return tf.nn.max_pool(inp, ksize, strides, padding)
+    
+out = maxpool(X)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    output = sess.run(out)
+    print(output)
+    print('Shape: ' + str(output.shape))
